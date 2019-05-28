0b9d29d855e483aa098021af868f27cfaacf97dd
Dingxin Zhang <dingxin0415@gmail.com>
2019-05-28 11:41:32 +1000

    v f0.8

--- web/api.py
+++ web/api.py
@@ -58,7 +58,7 @@ def insert_message(port, username, recipientname, massage_content):
 
     #massage_content.replace("'","")
     #massage_content.replace(";","")
-    massage_content = massage_content.repalce("<", "< ")
+    #massage_content = massage_content.repalce("<", "< ")
     output = requests.post(db_url + '/db/insert_message/' + username + '/' + recipientname + '/' + massage_content)
     output = decode(output)
 
